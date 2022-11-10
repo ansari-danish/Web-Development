236addc154a35f55f1ed3787ea224d8aa5dddd6d
Danish <dan71ish@gmail.com>
2022-11-10 11:13:36 +0530

    Search Functionality added

--- js cheatsheet/projects/superHeroApi/script.js
+++ js cheatsheet/projects/superHeroApi/script.js
@@ -3,13 +3,14 @@ const newHeroBtn = document.getElementById('hero-btn')
 
 const heroImage = document.getElementById('heroimage')
 
+const searchInput = document.getElementById('search')
 
 const superToken = '494510842606756'
 const baseUrl = `https:www.superheroapi.com/api.php/${superToken}`
 const getRandomhero = (id,name) => {
     fetch(`${baseUrl}/${id}`).then(response => response.json()).then(json => {       
 console.log(json)
-heroImage.innerHTML += `<img src="${json.image.url}"height=300px width =20%/>`
+ heroImage.innerHTML += `<img src="${json.image.url}"height=300px width =9%/>`
 })
 }
 
@@ -17,8 +18,23 @@ heroImage.innerHTML += `<img src="${json.image.url}"height=300px width =20%/>`
 
 const Random = () => {
     const number = 730;
-    return Math.ceil(Math.random() * number);
+    return Math.ceil(Math.random() * number)+1;
 }
-newHeroBtn.onclick = () => getRandomhero(Random());
+newHeroBtn.onclick = () => {
+    for(let i =0;i<10;i++){
 
+        getRandomhero(Random());
+    }
+}
 
+const SearchSuperhero = (name) => {
+    fetch(`${baseUrl}/search/${name}`).then(response => response.json()).then(json => {
+        const hero = json.results[0]
+        console.log(hero)
+        heroImage.innerHTML = `<img src= "${hero.image.url}" height=300px width=9% />`
+    })
+}
+const searchBtn = document.getElementById('search-btn')
+searchBtn.onclick = () => {
+    SearchSuperhero(searchInput.value)
+}
