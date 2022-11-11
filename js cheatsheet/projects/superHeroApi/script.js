501b960288cae2a09ce5cd04d4ddd23b4d0f1374
Danish <dan71ish@gmail.com>
2022-11-11 09:40:32 +0530

    Added search Functionality also

--- js cheatsheet/projects/superHeroApi/script.js
+++ js cheatsheet/projects/superHeroApi/script.js
@@ -8,9 +8,11 @@ const searchInput = document.getElementById('search')
 const superToken = '494510842606756'
 const baseUrl = `https:www.superheroapi.com/api.php/${superToken}`
 const getRandomhero = (id,name) => {
-    fetch(`${baseUrl}/${id}`).then(response => response.json()).then(json => {       
-console.log(json)
- heroImage.innerHTML += `<img src="${json.image.url}"height=300px width =9%/>`
+    fetch(`${baseUrl}/${id}`).then(response => response.json()).then(json => {    
+        console.log(json.powerstats) 
+        const superhero  = json;  
+        showHeroinfo(superhero);
+
 })
 }
 
@@ -21,20 +23,33 @@ const Random = () => {
     return Math.ceil(Math.random() * number)+1;
 }
 newHeroBtn.onclick = () => {
-    for(let i =0;i<10;i++){
-
         getRandomhero(Random());
     }
-}
+
+
+const showHeroinfo = (character) => {
+        const name = `<h2>${character.name}</h2>`
+
+         const img = `<img src="${character.image.url}"height=350px width =200px/>`
+
+            const stats = Object.keys(character.powerstats).map( stat => {
+            return `<p>${stat.toUpperCase()}  : ${character.powerstats[stat]}</p>`
+        }).join('')
+        console.log(name)
+        heroImage.innerHTML = `${name}${img}${stats}` 
+        return stats.join('');
+    }
 
 const SearchSuperhero = (name) => {
     fetch(`${baseUrl}/search/${name}`).then(response => response.json()).then(json => {
         const hero = json.results[0]
-        console.log(hero)
-        heroImage.innerHTML = `<img src= "${hero.image.url}" height=300px width=9% />`
+        showHeroinfo(hero)
+        // heroImage.innerHTML = `<img src= "${hero.image.url}" height=350px width=200px />`
     })
 }
-const searchBtn = document.getElementById('search-btn')
-searchBtn.onclick = () => {
+searchInput.onkeyup = () => {
     SearchSuperhero(searchInput.value)
 }
+
+
+
