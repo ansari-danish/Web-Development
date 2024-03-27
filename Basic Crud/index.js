799d4482d9d701d33c1b4f5b68cb2cf4f8c42602
Danish <dan71ish@gmail.com>
2024-03-28 00:04:58 +0530

    converted json server into a mongo db in the terminal

--- Basic Crud/index.js
+++ Basic Crud/index.js
@@ -1,12 +1,47 @@
 const express = require("express");
-const data = require("./MOCK_DATA.json");
+const mongoose = require("mongoose");
 const { json } = require("express");
-const fs = require("fs");
-const { isFunction } = require("util");
 
 const app = express();
 const PORT = 8000;
 app.use(json());
+app.use(express.urlencoded());
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/crud")
+  .then(() => {
+    console.log("Mongo Connected Successfully");
+  })
+  .catch((err) => {
+    console.log("Error while connecting", err);
+  });
+
+const userScheme = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    jobTitle: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("user", userScheme);
 
 app.get("/", (req, res) => {
   return res.send("Hello World");
@@ -16,83 +51,65 @@ app.listen(PORT, () => {
   console.log(`App is running on ${PORT}`);
 });
 
-app.get("/users", (req, res) => {
-  const ul = data.map((item) => {
-    return `<li>${item.first_name}</li>`;
-  });
+app.get("/users", async (req, res) => {
+  const allData = await User.find({});
+  const ul = allData
+    .map((item) => {
+      return `<li>${item.first_name} ${item.email}</li>`;
+    })
+    .join("");
   const list = `<ul>${ul}</ul>`;
   return res.send(list);
 });
 
-app.get("/api/users", (req, res) => {
-  return res.status(200).json(data);
+app.get("/api/users", async (req, res) => {
+  const allUsers = await User.find({});
+  return res.status(200).json(allUsers);
 });
 
-app.get("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const finalArr = data.find((item) => item.id === id);
+app.get("/api/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const finalArr = await User.findById(id);
+  if (!finalArr) {
+    return res.status(403).json("User doesn't exist");
+  }
   return res.send(finalArr);
 });
 
-app.post("/api/users", (req, res) => {
-  const { first_name, last_name, email, gender, wealth } = req.body;
-  const id = data.length + 1;
-  const dataObj = {
-    id: id,
+app.post("/api/users", async (req, res) => {
+  const { first_name, last_name, email, gender, jobTitle } = req.body;
+  console.log({ first_name, last_name, email, gender, jobTitle });
+
+  if (!first_name || !last_name || !email || !gender || !jobTitle) {
+    return res.status(403).json("Data is not send");
+  }
+
+  await User.create({
     first_name: first_name,
     last_name: last_name,
     email: email,
     gender: gender,
-    wealth: wealth,
-  };
-  data.push(dataObj);
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(data), (err) => {
-    if (err) {
-      return res.send("Error");
-    } else {
-      return res.send("Success");
-    }
+    jobTitle: jobTitle,
   });
+
+  return res.send("Success");
 });
 
-app.delete("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const finalArr = data.filter((item) => item.id !== id);
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(finalArr), (err) => {
-    if (err) {
-      return res.send("Error");
-    } else {
-      return res.send("Success");
-    }
-  });
+app.delete("/api/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const finalArr = await User.findByIdAndDelete(id);
+  return res.status(200).send("Success");
 });
 
-app.patch("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
+app.patch("/api/users/:id", async (req, res) => {
+  const id = req.params.id;
   const { first_name, last_name, email, gender, wealth } = req.body;
-  const findArr = data.find((item) => item.id === id);
-  const index = data.indexOf(findArr);
-  if (first_name) {
-    findArr.first_name = first_name;
-  }
-  if (last_name) {
-    findArr.last_name = last_name;
-  }
-  if (email) {
-    findArr.email = email;
-  }
-  if (gender) {
-    findArr.gender = gender;
-  }
-  if (wealth) {
-    findArr.wealth = wealth;
-  }
-  data[index] = findArr;
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(data), (err) => {
-    if (err) {
-      return res.send("Error");
-    } else {
-      return res.send("Success");
-    }
+  const response = await User.findByIdAndUpdate(id, {
+    first_name,
+    last_name,
+    email,
+    gender,
+    wealth,
   });
+  return res.send(response);
 });
